bdf5d409c65829062ac70947c7a53566aa59efb1
Anderson Rosa <anderson.lucasrosa@gmail.com>
2019-06-10 18:46:48 -0300

    master

--- app/database.php
+++ app/database.php
@@ -1,14 +1,89 @@
 <?php
 
-const DB_HOST = '192.168.0.14';
+const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASS = 'root';
+const DB_PASS = '';
 const DB_NAME = 'db_locashow';
-const DB_PORT = 6603;
 
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$mysqli->set_charset("utf8");
 
 if (mysqli_connect_errno()) {
     printf("Conexão com o banco de dados falhou: %s\n", mysqli_connect_error());
     exit;
+}
+
+function get($sql)
+{
+    global $mysqli;
+
+    $return = [];
+    if ($result = $mysqli->query($sql)) {
+      while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
+        $return[] = $row;
+      }
+      $result->free();
+    }
+
+    return $return;
+}
+
+function set($table, $data)
+{
+    global $mysqli;
+
+    if (isset($data['id']) && $data['id'] != 0) {
+        $sql = "UPDATE {$table} SET ";
+        $i = 1;
+        foreach ($data as $key => $value) {
+            $sql .= "{$key} = '{$value}'";
+            if ($i != count($data)) $sql .= ', ';
+            $i++;
+        }
+        $sql .= " WHERE id = {$data['id']}";
+    } else {
+        $sql = "INSERT INTO {$table} ";
+        $fields = '(';
+        $values = '(';
+        $i = 1;
+        foreach ($data as $key => $value) {
+            $fields .= "{$key}";
+            $values .= "'{$value}'";
+            if ($i != count($data)) {
+                $fields .= ', ';
+                $values .= ', ';
+            }
+            $i++;
+        }
+        $sql .= "{$fields}) VALUES {$values})";
+    }
+
+    $status = 200;
+    $message = [];
+    if (! $mysqli->query($sql)) {
+        $message = 'Erro ao inserir! ERRO: '.$mysqli->error;
+        $status = 400;
+    } else {
+        $message = 'Sucesso!';
+    }
+
+    return ['message' => $message, 'status' => $status];
+}
+
+function delete($table, $where)
+{
+    global $mysqli;
+
+    $sql = "DELETE FROM {$table} {$where}";
+
+    $status = 200;
+    $message = [];
+    if (! $mysqli->query($sql)) {
+        $message = 'O registro não pode ser deletado!';
+        $status = 400;
+    } else {
+        $message = 'Deletado com sucesso!';
+    }
+
+    return ['message' => $message, 'status' => $status];
 }
