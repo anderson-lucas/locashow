0d5977f181f71f0ae61707a54165e2703c95ab1c
Anderson Rosa <anderson.lucasrosa@gmail.com>
2019-06-13 13:12:01 -0300

    master

--- app/database.php
+++ app/database.php
@@ -1,16 +1,16 @@
 <?php
 
-const DB_HOST = '192.168.0.14';
-const DB_USER = 'root';
-const DB_PASS = 'root';
-const DB_NAME = 'db_locashow';
-const DB_PORT = 6603;
-
-// const DB_HOST = 'localhosw';
+// const DB_HOST = '192.168.0.14';
 // const DB_USER = 'root';
-// const DB_PASS = '';
+// const DB_PASS = 'root';
 // const DB_NAME = 'db_locashow';
-// const DB_PORT = 3306;
+// const DB_PORT = 6603;
+
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'db_locashow';
+const DB_PORT = 3306;
 
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 $mysqli->set_charset("utf8");
